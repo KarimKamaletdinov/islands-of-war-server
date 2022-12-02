94c7d9c1bddfe908148c57ad9d3459eca65978e5
karim <450906k@gmail.com>
2022-12-02 22:15:23 +0500

    create app; init Flask

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+from app import app
+from os import environ
+from db import init, execute
+
+
+init(dict(environ))
+execute('CREATE TABLE test (id INTEGER, name VARCHAR)')
+app.run()
